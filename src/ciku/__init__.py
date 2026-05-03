6fc509f6172eac94398844c106863f4ef63011f5
hantang <34468720+hantang@users.noreply.github.com>
2026-05-03 19:52:22 +0800

    Bump version: 0.3.0 → 0.3.1

--- src/ciku/__init__.py
+++ src/ciku/__init__.py
@@ -1,7 +1,7 @@
 from .parser import SogouParser, BaiduParser, BaiduMobileParser, QQParser, QQV1Parser, HuayuParser
 from .parser import BaiduPCParser, QQV2Parser
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = [
     "SogouParser",
